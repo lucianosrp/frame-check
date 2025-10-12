985a61d50a146e12e9df5eda4486aa6dc7661f6e
Howard KANG <tinghaokang@gmail.com>
2025-10-12 15:33:01 +0800

    +unit test for [BUG] Keys as variable names raise parsing error #37

--- frame-check-core/tests/test_frame_init.py
+++ frame-check-core/tests/test_frame_init.py
@@ -30,3 +30,17 @@ df = pd.DataFrame(data)
     assert frame_instance.id == "df"
     assert frame_instance.columns == ["a", "b"]
     assert frame_instance.lineno == 5
+
+def test_frame_init_header_as_str_var():
+    code = """
+import pandas as pd
+col1 = "a"
+df = pd.DataFrame({col1: [1, 2, 3], "b": [4, 5, 6]})
+"""
+    fc = FrameChecker.check(code)
+    assert fc.frames.instance_keys() == ["df"]
+    frame_instance = fc.frames.get_at(4, "df")
+    assert frame_instance is not None
+    assert frame_instance.id == "df"
+    assert frame_instance.columns == ["a", "b"]
+    assert frame_instance.lineno == 4
