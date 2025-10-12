5ec3bf616fa794dc8fe93d4ea5c66063c946d380
Ethan Li <ejml1@st-andrews.ac.uk>
2025-10-12 16:55:31 +0800

    Remove comments and unused code

--- frame-check-core/tests/test_frame_init.py
+++ frame-check-core/tests/test_frame_init.py
@@ -1,5 +1,3 @@
-import pytest
-
 from frame_check_core import FrameChecker
 
 def test_frame_init_dict_arg():
@@ -16,7 +14,6 @@ df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
     assert frame_instance.columns == ["a", "b"]
     assert frame_instance.lineno == 4
 
-# @pytest.mark.xfail(reason="Not implemented yet")
 def test_frame_init_list_of_dict_arg():
     code = """
 import pandas as pd
