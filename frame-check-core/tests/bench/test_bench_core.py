f3ab328974d065f399e5269624d5c6e04bff690d
Alex Au <79151747+alex-au-922@users.noreply.github.com>
2025-10-14 15:08:38 +0800

    Changing hardcoded benchmark file path to relative path to ensure correct benchmarks on different execution paths (#56)

--- frame-check-core/tests/bench/test_bench_core.py
+++ frame-check-core/tests/bench/test_bench_core.py
@@ -1,7 +1,9 @@
 import pytest
 from frame_check_core import FrameChecker
+from pathlib import Path
 
 
 @pytest.mark.benchmark
 def test_long_benchmark():
-    FrameChecker.check("frame-check-core/tests/bench/long_pandas.py")
+    benchmark_file = Path(__file__).parent / "long_pandas.py"
+    FrameChecker.check(benchmark_file)
