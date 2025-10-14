ba50c7315fd0e33fc79cf3c09f3264c9a35197fc
tonyyuyiding <tonyyuyiding@gmail.com>
2025-10-14 21:46:40 +0800

    fix zero_deps_jaro_winkler

--- frame-check-core/src/frame_check_core/util/col_similarity.py
+++ frame-check-core/src/frame_check_core/util/col_similarity.py
@@ -55,12 +55,11 @@ def zero_deps_jaro_winkler(target_col, existing_cols):
         col: abs(jaro_winkler(target_col, col)) for col in existing_cols
     }
 
-    target_value = max(
-        jw_distances_dict.values()
-    )  # FIXME: this raises an error if empty
-    if target_value > 0.9:
-        index = list(jw_distances_dict.values()).index(target_value)
-        result = list(jw_distances_dict.keys())[index]
-        return f"Column '{target_col}' does not exist, did you mean '{result}'?"
-    else:
-        return f"Column '{target_col}' does not exist"
+    if jw_distances_dict.values():
+        target_value = max(jw_distances_dict.values())
+        if target_value > 0.9:
+            index = list(jw_distances_dict.values()).index(target_value)
+            result = list(jw_distances_dict.keys())[index]
+            return f"Column '{target_col}' does not exist, did you mean '{result}'?"
+
+    return f"Column '{target_col}' does not exist"
